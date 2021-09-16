d94488c034d02f635829cd23fc6e56dee58ba81d
Henry Rocha <henryrocha@protonmail.com>
2021-09-16 18:45:37 -0300

    feat: add basic localization

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 from dev_aberto import hello
+import datetime
+import babel.dates
+import gettext
 
 if __name__ == "__main__":
+    gettext.install("cli", localedir="locale")
     date, name = hello()
-    print("Último commit feito em:", date, " por", name)
+    # Parse date from string.
+    date = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ").date()
+    print(_("Last commit at:"), babel.dates.format_datetime(date), _("authored by"), name)
